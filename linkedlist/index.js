2f95b21b6c41faf9d267ae12ef39fa3e5c507ca5
aSamurai <xhc044x@gmail.com>
2017-09-20 01:26:37 +0300

    Add initial code for linkedlist data structure js implementation

---
+++ linkedlist/index.js
@@ -0,0 +1,62 @@
+/**
+ * Linked List is a sequence of links which contains items. 
+ * Each link contains a connection to another link. 
+ * Linked list is the second most-used data structure after array. 
+ * Following are the important terms to understand the concept of Linked List.
+ * 
+ * @class LinkedList
+ * 
+ * @method add    :
+ * @method remove :
+ * @method get    :
+ * 
+ * @see http://www.geeksforgeeks.org/linked-list-set-1-introduction/
+ */
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.length = 0;
+    }
+    add(value, position) {
+        const node = {
+            value: value,
+            next: null
+        };
+    
+        if (position === 0) {
+            this.head = node;
+        } else {
+              const prev = this.get(position - 1);
+              const current = prev.next;
+              node.next = current;
+              prev.next = node;
+        }
+    
+        ++this.length;
+        
+        return this;
+    }
+    remove(position) {
+        if (position === 0) {
+            this.head = this.head.next;
+        } else {
+            const prev = this.get(position - 1);
+            prev.next = prev.next.next;
+        }
+
+        --this.length;
+        
+        return this;
+    }
+    get(position) {
+        if (position > this.length) throw new Error('Position is invalid.');
+        if (this.length === 0) return null;
+
+        let current = this.head;
+        for (let index = 0; index < position; index++) {
+            current = current.next;
+        }
+
+        return current;
+    }
+}
